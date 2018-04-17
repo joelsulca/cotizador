4681659128fe895c53dd7437a0b28315f548d30b
holivares <heraldmatias.oz@gmail.com>
2018-04-17 16:30:21 -0500

    cotizacion flow

--- bin/server.sh
+++ bin/server.sh
@@ -8,5 +8,5 @@
 ## @link       https://github.com/kenjis/codeigniter-composer-installer
 
 cd `dirname $0`/..
-export SENDGRID_API_KEY=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
-/c/xampp5.6/php/php -S 127.0.0.1:8000 -t public -f bin/router.php
+
+php -S 127.0.0.1:8000 -t public -f bin/router.php
